04394c9844f45d6387b0d3a9a8df3ecf111f0ece
rahul-chouhan-wm <lucifer243607@gmail.com>
2022-09-01 15:22:05 +0530

    modules , exception handling,bubble and quick sort

--- LearningRuby.rb
+++ LearningRuby.rb
@@ -36,7 +36,7 @@
 
 # element = []
 
-# (0...4).each do |i|                              #0..4 p from 0 to 4 but 0...4 p from 0 to 3 leaves the high value
+# (0...4).each do |i|                              #0..4 p from 0 to 4      but 0...4 p from 0 to 3 leaves the high value
 #     #element.push(i)
 #     element<<i
 # end
@@ -158,6 +158,110 @@
 # email = EmailConnector.new("SecondTitle","this is my second time writing","somerandomurl.com")
 # email.same_name
 
+#----------------------modules with class and exception handling--------------------------
+# module SomeData 
+#     module AfterSomeData
+#         class << self
+#             def some_data(params)
+#                 data = params[:data]
+#                 number = params[:number]
+#                 puts "This is data : #{data}"
+#                 squareAns = Square(number)
+#                 puts squareAns
+#                 begin
+#                     divisionAns = divide(number)
+#                     puts divisionAns
+#                 rescue ZeroDivisionError => e
+#                     puts "There is an error :#{e}" 
+#                 end
+#             end
 
+#             private
+#             def Square(number)
+#                 number * number
+#             end
+#             private
+#                 def divide(number)
+#                     number/0
+#                 end
+#         end
+#     end
+# end
+# params_data = {
+#     data:"raw information without any meaning is callled data.",
+#     number: 2
+# }
+
+# SomeData::AfterSomeData.some_data(params_data)
+
+#------------------------meta programming-----------------
+# class String
+#     def censor(words)
+#         self.gsub! "#{words}" , "Removed"
+#     end
+# end
+# p "A rabbit dugs a hole in ground to live(also to survive).".censor("to")
+
+# require 'ostruct'
+
+# class Author
+#     attr_accessor :first_name , :last_name , :genre
+
+#     def Author
+#         OpenStruct.new(first_name:first_name , last_name:last_name , genre:genre)
+#     end
+
+#     def method_missing(method_name, *arguments , &block)
+#         if method_naem.to_s =~ /author.(.*)/
+#             author.send($1,*arguments ,&block)
+#         else
+#             super
+#         end
+#     end
+# end
+
+# author = Author.new
+# author.first_name="Cal"
+# author.last_name="Newport"
+# author.genre = "Computer engineer"
+
+# p author.author_genre
+
+#---------------------------------------------------Bubble sort in ruby-------------------------------------------
+
+# arr= [23,1,34,69,12,9]
+
+# def bubble_sort (array)
+#     n=array.length
+
+#     loop do 
+#         swapped = false
+#         (n-1).times do |i|
+#             if array[i]> array[i+1]
+#                 array[i] , array[i+1]= array[i+1] , array[i]
+#                 swapped = true
+#             end
+#         end
+#         break if not swapped
+#     end
+#     array
+# end
+
+# p bubble_sort(arr)
+
+#-----------------------------Quick sort -------------------------------
+class Array
+    def quick_sort
+        return [] if empty?
+        pivot = delete_at(rand(size))
+        left , right = partition(&pivot.method(:>))
+        
+        return [*left.quick_sort , pivot , *right.quick_sort]
+    end
+end
+
+arr=[12,2,9,56,45,23,34,1,7,9,1]
+
+p arr.quick_sort
 
 
